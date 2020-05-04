0ca03e151d7cc7dd29d57b767b6ac09358e8677a
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-05-04 16:16:52 +0200

    Added more options to command line

--- addon_pack_interface.py
+++ addon_pack_interface.py
@@ -6,8 +6,36 @@ def npc_options():
     if os.path.exists("names_merged.xlsx"):
         df_arg = pd.read_excel("names_merged.xlsx")
         print("Loading NPC options....")
+
         print("Type the number of NPC's you wish to create: ")
-        npc_num = input(": ")
+        npc_num = None
+        while npc_num is None:
+            try:
+                num_arg = int(input(""))
+                if num_arg > 101 or num_arg < 0:
+                    print("The program can only create less than 100 NPC's, try again")
+                else:
+                    npc_num = num_arg
+            except:
+                print("There was an error, please ensure the input is a valid number")
+        print("Are the NPC's the same culture as each other? [y/n]")
+        culture_arg = None
+        yes_list, no_list = ["y", "yeh", "yes", "yep"], ["n", "no", "nah", "nope"]
+        while culture_arg is None:
+            try:
+                culture_arg = input("")
+                if culture_arg.lower() in yes_list:
+                    print("Creating NPC's with the same culture")
+                    culture_arg = "yes"
+                elif culture_arg.lower() in no_list:
+                    print("Creating NPC's with different cultures")
+            except:
+                print("There was an error, please ensure the input corresponds to yes or no")
+
+        for i in range(npc_num):
+            break
+
+
 
     else:
 
