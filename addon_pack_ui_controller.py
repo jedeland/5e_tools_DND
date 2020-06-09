04f2760eb36da7a6f3dbb1a8c00ec2417f1e4695
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-06-09 16:18:35 +0200

    Refactored and added overhead menu's

--- addon_pack_ui_controller.py
+++ addon_pack_ui_controller.py
@@ -8,18 +8,36 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         self.setWindowTitle("Main Menu")
+        self.create_menu()
+        #self.menu = self.menuBar()
+        #self.option = self.menu.addMenu("File")
+        #exit_action = QAction("Exit", self)
 
-        self.menu = self.menuBar()
-        self.option = self.menu.addMenu("File")
-        exit_action = QAction("Exit", self)
-        exit_action.setShortcut(QKeySequence.Quit)
-        exit_action.triggered.connect(self.close)
+        #exit_action.setShortcut(QKeySequence.Quit)
+        #exit_action.triggered.connect(self.close)
 
-        self.option.addAction(exit_action)
+        #self.option.addAction(exit_action)
         self.status = self.statusBar()
+        #self.option = self.menu.addMenu("Tools")
+        #mutate_action = QAction("Mutate Monster", self)
+
+        #mutate_action = QAction()
+
         self.status.showMessage("Data loaded")
 
+    def create_menu(self):
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("File")
+        toolMenu = mainMenu.addMenu("Tools")
+        downloadMenu = mainMenu.addMenu("Save Output")
 
+        quitAction = QAction("Exit" , fileMenu)
+        quitAction.setShortcut(QKeySequence.Quit)
+        quitAction.triggered.connect(self.close)
+        fileMenu.addAction(quitAction)
+
+        mutateAction = QAction("Mutate Monster", toolMenu)
+        toolMenu.addAction(mutateAction)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
