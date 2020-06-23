3a5e22076ab7ac292f885fd0a17421cf697b99ae
JEland-Git-it-done <57952879+JEland-Git-it-done@users.noreply.github.com>
2020-06-23 15:57:40 +0200

    Testing out pheasability of localized round timer

---
+++ addon_pack_roundtimer.py
@@ -0,0 +1,41 @@
+# Python program to implement client side of chat room. 
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print
+    "Correct usage: script, IP address, port number"
+
+IP_address = str("127.0.0.1")
+print(IP_address)
+Port = int(4455)
+server.connect((IP_address, Port))
+
+while True:
+
+    # maintains a list of possible input streams 
+    sockets_list = [socket.socket(), server]
+
+    """ There are two possible input situations. Either the 
+    user wants to give  manual input to send to other people, 
+    or the server is sending a message  to be printed on the 
+    screen. Select returns from sockets_list, the stream that 
+    is reader for input. So for example, if the server wants 
+    to send a message, then the if condition will hold true 
+    below.If the user wants to send a message, the else 
+    condition will evaluate as true"""
+    read_sockets, write_socket, error_socket = select.select(sockets_list, [], [])
+
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            server.send(message)
+            server.send(b"<You>")
+            server.send(b"{}".format(message))
+            #sys.stdout.flush()
+server.close()
